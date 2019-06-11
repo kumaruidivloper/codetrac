e665c9f4dc7be457da5537e01c8ecb40735c191a
kumarshan <kumaruidivloper@gmail.com>
2019-06-11 15:19:27 +0530

    create-rest-api-with-data

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 const mongoose = require('mongoose');
 const db = "mongodb+srv://kumar:kumar1234@cluster0-t6xml.mongodb.net/test?retryWrites=true"
@@ -17,5 +18,61 @@ router.get('/', (req, res) => {
     res.send('From API Route')
 })
 
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.status(200).sent(registeredUser)
+        }
+    })
+});
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+
+    User.findOne({email: userData.email}, (error, user) => {
+        if (error) {
+            console.log(error);
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid Email')
+            } else {
+                if (user.password !== userData.password) {
+                    res.status(401).send('Invalid Password')
+                } else {
+                    res.status(200).send(user)
+                }
+            }
+        }
+    })
+})
+
+router.get('/dashboard', (req, res) => {
+    let dashboard = [
+        {
+            "id": 1,
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "id": 2,
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "id": 3,
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        }
+    ]
+
+    res.json(dashboard)
+})
 
 module.exports = router
